59d9205867cb921e361d465059843da1467fee72
Ana1775 <anakarenrosendo273@gmail.com>
2025-07-28 17:00:45 -0600

    Update eliminar_libro.php
    Al eliminar un libro, también se elimina su portada del servidor si fue subida localmente.

--- eliminar_libro.php
+++ eliminar_libro.php
@@ -1,13 +1,11 @@
 <?php
 session_start(); 
-
+require_once 'db.php';
 // Verifica si el usuario está autenticado
 if (!isset($_SESSION['usuario'])) {
     header("Location: login.php"); // Si no lo esta, redirige al login
     exit();
 }
-
-require_once 'db.php';
 
 // Obtiene el ID del libro desde la URL usando GET
 $id = $_GET['id'] ?? null; 
@@ -17,7 +15,20 @@ if (!$id) {
     echo "ID no válido.";
     exit();
 }
+//se elimina la portada si se guardo en el servidor
+$stmt = $conn->prepare("SELECT portada FROM libros WHERE id = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$stmt->bind_result($portada);
+$stmt->fetch();
+$stmt->close();
 
+if (!empty($portada) && !filter_var($portada, FILTER_VALIDATE_URL) && $portada !== 'imagenes/iconos/icono_portada.avif'
+) {
+    if (file_exists($portada)) {
+        unlink($portada);
+    }
+}
 // Prepara la consulta SQL para eliminar el libro con el ID recibido
 $stmt = $conn->prepare("DELETE FROM libros WHERE id = ? AND usuario_id = ?");
 $stmt->bind_param("ii", $id, $_SESSION['usuario']['id']); 
